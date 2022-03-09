41c125845499ab4c3c6485c34db2a1634110a6c5
doanyennhi <s3880599@rmit.edu.vn>
2022-03-10 02:09:41 +0700

    Setup project structure and some global styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import "./styles/global.css"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="para">sss</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
