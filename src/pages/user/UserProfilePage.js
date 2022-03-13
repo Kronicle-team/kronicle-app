0cc59c8911d88a2c84999a3a9b223f01d0aa2c71
rider3458 <s3878480@rmit.edu.vn>
2022-03-13 12:27:10 +0700

    add avatar div to user-profile page

--- src/pages/user/UserProfilePage.js
+++ src/pages/user/UserProfilePage.js
@@ -1,25 +1,50 @@
 import Layout from "../../components/Layout";
 
 const UserProfilePage = () => {
-    return () => {
-        <Layout>
-            <article>
-                    <ul class="my-account-info">
-                    <li><label>Username</label></li>
-                    <li><label>First Name</label></li>
-                    <li><label>Last Name</label></li>
-                    <li><label>Gender</label></li>
-                    <li><label>Birthdate</label></li>
-                    <li><label>Email</label></li>
-                    <li><label>Phone Number</label></li>
-                    <li><label>Address</label></li>
-                    <li><label>City - Zipcode</label></li>
-                    <li><label>Country</label></li>
-                </ul>
-                <button>Edit</button>
-            </article>
-        </Layout>
-    }
-}
+  return () => {
+    <Layout>
+      <article>
+        <ul class="my-account-info">
+          <li>
+            <label>Username</label>
+          </li>
+          <li>
+            <label>First Name</label>
+          </li>
+          <li>
+            <label>Last Name</label>
+          </li>
+          <li>
+            <label>Gender</label>
+          </li>
+          <li>
+            <label>Birthdate</label>
+          </li>
+          <li>
+            <label>Email</label>
+          </li>
+          <li>
+            <label>Phone Number</label>
+          </li>
+          <li>
+            <label>Address</label>
+          </li>
+          <li>
+            <label>City - Zipcode</label>
+          </li>
+          <li>
+            <label>Country</label>
+          </li>
+        </ul>
+        <button>Edit</button>
+      </article>
+      <article>
+        <div>
+          <img alt="profile picture" />
+        </div>
+      </article>
+    </Layout>;
+  };
+};
 
 export default UserProfilePage;
