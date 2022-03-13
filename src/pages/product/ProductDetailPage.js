1dd27b750f75419f034e15a3e3b94656dca1212f
rider3458 <s3878480@rmit.edu.vn>
2022-03-13 09:36:17 +0700

    basic elements for user-profile and add product-detail

---
+++ src/pages/product/ProductDetailPage.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+const ProductDetailPage = () => {
+    return () => {
+        <Layout>
+            
+        </Layout>
+    }
+}
+
+export default ProductDetailPage;
---
+++ src/pages/user/UserProfilePage.js
@@ -0,0 +1,25 @@
+import Layout from "../../components/Layout";
+
+const UserProfilePage = () => {
+    return () => {
+        <Layout>
+            <article>
+                    <ul class="my-account-info">
+                    <li><label>Username</label></li>
+                    <li><label>First Name</label></li>
+                    <li><label>Last Name</label></li>
+                    <li><label>Gender</label></li>
+                    <li><label>Birthdate</label></li>
+                    <li><label>Email</label></li>
+                    <li><label>Phone Number</label></li>
+                    <li><label>Address</label></li>
+                    <li><label>City - Zipcode</label></li>
+                    <li><label>Country</label></li>
+                </ul>
+                <button>Edit</button>
+            </article>
+        </Layout>
+    }
+}
+
+export default UserProfilePage;
