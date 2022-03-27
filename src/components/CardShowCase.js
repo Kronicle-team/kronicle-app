6916cbf12c3c62a03c4ea8a558d9719cb43fb329
rider3458 <s3878480@rmit.edu.vn>
2022-03-27 13:34:36 +0700

    lower the number of cards depended on the width of the page, back and forward functions for card showcase

--- src/components/CardShowCase.js
+++ src/components/CardShowCase.js
@@ -3,21 +3,44 @@ import React from "react";
 import ProductCard from "./ProductCard";
 import ToggleButton from "./ToggleButton";
 import style from "./CardShowCase.module.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const CardShowCase = (props) => {
+    let maxItemsPerPage = 4
+ if (window.innerWidth < 1270) {
+     maxItemsPerPage--
+ }
+ if (window.innerWidth < 962) {
+     maxItemsPerPage--
+ }
+ if (window.innerWidth < 655) {
+     maxItemsPerPage--
+ }
+
+const [page, setPage] = useState(0)
+let totalPage = Math.ceil(props.data.length / maxItemsPerPage)
+
  return (
-     <div>
-         <p>{props.name}</p>
-         <div className={style.cardShowCaseContainer}>
-            <ToggleButton icon={faChevronLeft} className={style.prevBtn}/>
-             <div className={style.cardWrapper}>
-                {props.data.map((card) => {
-                    return (
+     <div className={style.cardShowCaseContainer}>
+         <div className={style.cardShowCaseTitle}>
+             <h1>{props.name} <span><Link to={"/all"} className={style.seeMoreLink}>See more</Link></span></h1>
+         </div>
+         <div className={style.prevBtn}>
+           <button onClick={() => {setPage( (page - 1) % totalPage)}}><ToggleButton icon={faChevronLeft} /></button>
+         </div>
+         <div className={style.cardWrapper}>
+             {
+                 props.data.slice(page * maxItemsPerPage,(maxItemsPerPage * (page+1))).map((card) => {
+                     console.log("page: " + page)
+                     console.log("card id: " + props.data.indexOf(card))
+                     return (
                         <ProductCard key={card.id} img={card.img} name={card.name} price={card.price}/>
-                    )
-                })}
-             </div>
-            <ToggleButton icon={faChevronRight} className={style.forwardBtn}/>
+                     )
+             })}
+         </div>
+         <div  className={style.forwardBtn}>
+            <button  onClick={() => {setPage( (page + 1) % totalPage)}}><ToggleButton icon={faChevronRight}/></button>
          </div>
      </div>
  )
