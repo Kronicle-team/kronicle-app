67315d2f5cb963047485e6b18704eff8c9b7ecce
doanyennhi <s3880599@rmit.edu.vn>
2022-04-01 00:12:58 +0700

    Change global font size, more styling for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <footer className={style["footer"]}>
         <div className={style["footer-nav"] + " " + common["flex"]}>
           <div>
-            <h4>CATEGORIES</h4>
+            <h5>CATEGORIES</h5>
             <Link to="/cards/album-cards">Album cards</Link>
             <Link to="/cards/trading-cards">Trading cards</Link>
             <Link to="/cards/buy-now">Buy now</Link>
@@ -17,7 +17,7 @@ const Footer = () => {
           </div>
 
           <div>
-            <h4>CUSTOMER CARE</h4>
+            <h5>CUSTOMER CARE</h5>
             <Link to="/">Tracking My Order</Link>
             <Link to="/">Privacy Policy</Link>
             <Link to="/">Shipping Policy</Link>
@@ -25,17 +25,24 @@ const Footer = () => {
           </div>
 
           <div>
-            <h4>INFO</h4>
+            <h5>INFO</h5>
             <Link to="/">About Us</Link>
             <Link to="/">FAQ</Link>
             <Link to="/">Product Care</Link>
             <Link to="/">Terms And Conditions</Link>
           </div>
+
+          <div className={style["contact"]}>
+            <h5>HAVE ANY QUESTIONS?</h5>
+            <button className={style["contact-btn"]}>Contact Us</button>
+          </div>
         </div>
 
-        <div className={[common["text-center"], common["flex"]].join(" ")}>
+        <div className={[style["copyright"], common["flex"]].join(" ")}>
           &copy; {currentYear} We are Kronicle. All Rights Reserved.
         </div>
+
+
       </footer>
   )
 }
