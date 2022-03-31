3d92941163ecf2f6766225470451d34e9f197404
doanyennhi <s3880599@rmit.edu.vn>
2022-04-01 00:42:16 +0700

    Code header when user is login or when screen size is small

--- src/components/header/TopHeader.js
+++ src/components/header/TopHeader.js
@@ -3,9 +3,17 @@ import common from "../../styles/common.module.css";
 import {Link} from "react-router-dom";
 import SearchBar from "./SearchBar";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faArrowUpFromBracket, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowUpFromBracket,
+  faBars,
+  faShoppingCart,
+  faUserCircle
+} from "@fortawesome/free-solid-svg-icons";
 
 const TopHeader = () => {
+  const isLoggedIn = false;
+  const isWindowSize = false;
+
   return (
       <div className={[style["top-header"], common["flex"]].join(" ")}>
         <div className={[style["top-header-left"], common["flex"]].join(" ")}>
@@ -13,17 +21,45 @@ const TopHeader = () => {
             <img alt="Kronicle logo" src={"../media/icons/logo.png"} className={style["logo"]} />
           </Link>
 
-          <SearchBar />
-
-          <Link to="/"><FontAwesomeIcon icon={faArrowUpFromBracket} className={style["icon"]} /></Link>
-          <Link to="/"><FontAwesomeIcon icon={faShoppingCart} className={style["icon"]} /></Link>
+          {isWindowSize
+          ? (<></>)
+          : (
+              <>
+                <SearchBar />
+                <Link to="/"><FontAwesomeIcon icon={faArrowUpFromBracket} className={style["icon"]} /></Link>
+                <Link to="/"><FontAwesomeIcon icon={faShoppingCart} className={style["icon"]} /></Link>
+              </>
+              )
+          }
         </div>
 
-        <div className={[style["top-header-right"], common["flex"]].join(" ")}>
-          <Link to="/login">Login</Link>
-          <div className={style["vertical-line"]} />
-          <Link to="/register" className={style["register"]}>Register</Link>
-        </div>
+        {isWindowSize
+            ? (
+                <div className={[style["top-header-right"], common["flex"]].join(" ")}>
+                  <button className={style["menu"]}><FontAwesomeIcon icon={faBars}/></button>
+                </div>
+            )
+            : (
+                <div className={[style["top-header-right"], common["flex"]].join(" ")}>
+                  {isLoggedIn
+                      ? (
+                          <>
+                            <Link to="/" className={style["avatar"]}><FontAwesomeIcon icon={faUserCircle}/></Link>
+                            <Link to="/">Logout</Link>
+                          </>
+                      )
+                      : (
+                          <>
+                            <Link to="/login">Login</Link>
+                            <div className={style["vertical-line"]}/>
+                            <Link to="/register" className={style["register"]}>Register</Link>
+                          </>
+                      )
+                  }
+                </div>
+            )
+        }
+
       </div>
 
   )
