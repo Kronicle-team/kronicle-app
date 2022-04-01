316998636a9664b100ab5d9af47f474dffb78674
Thu Tran <ngocanhthu20102002@gmail.com>
2022-04-01 09:27:36 +0700

    added initial layout

--- src/components/form/ListingForm.js
+++ src/components/form/ListingForm.js
@@ -1,59 +1,32 @@
 /**
  * Listing form.
  */
-import React from 'react'
-import { Form, Field } from 'react-advanced-form'
-import { Input, Button } from 'react-advanced-form-addons'
+import { useState } from "react";
+import "./ListingForm.css";
 
-export default class RegistrationForm extends React.Component {
-    registerUser = ({ serialized, fields, form }) => {
-        return fetch('https://backend.dev', {
-            body: JSON.stringify(serialized)
-        })
-    }
+const FormInput = (props) => {
+    const [focused, setFocused] = useState(false);
+    const { label, errorMessage, onChange, id, ...inputProps } = props;
 
-    render() {
-        return (
-            <Form
-                action={this.registerUser}
-                onSubmitStart={this.props.onSubmitStart}>
-                <Field.Group name="primaryInfo">
-                    <Input
-                        name="userEmail"
-                        type="email"
-                        label="E-mail"
-                        required />
-                </Field.Group>
+    const handleFocus = (e) => {
+        setFocused(true);
+    };
 
-                <Input
-                    name="userPassword"
-                    type="password"
-                    label="Password"
-                    required />
-                <Input
-                    name="confirmPassword"
-                    type="password"
-                    label="Confirm password"
-                    required
-                    skip />
+    return (
+        <div className="formInput">
+            <label>{label}</label>
+            <input
+                {...inputProps}
+                onChange={onChange}
+                onBlur={handleFocus}
+                onFocus={() =>
+                    inputProps.name === "confirmPassword" && setFocused(true)
+                }
+                focused={focused.toString()}
+            />
+            <span>{errorMessage}</span>
+        </div>
+    );
+};
 
-                <Field.Group name="primaryInfo">
-                    <Input
-                        name="firstName"
-                        label="First name"
-                        required={({ get }) => {
-                            return !!get(['primaryInfo', 'lastName', 'value'])
-                        }} />
-                    <Input
-                        name="lastName"
-                        label="Last name"
-                        required={({ get }) => {
-                            return !!get(['primaryInfo', 'firstName', 'value'])
-                        }} />
-                </Field.Group>
-
-                <Button primary>Register</Button>
-            </Form>
-        );
-    }
-}
+export default FormInput;
---
+++ src/pages/listing/ListingPage.js
@@ -0,0 +1,87 @@
+import {useState} from "react";
+import FormInput from "../../components/form/ListingForm";
+
+const React = require("react")
+require("../../components/form/ListingForm.js")
+require("./ListingPage.css");
+
+
+const ListingPage = () => {
+    const [values, setValues] = useState({
+        productname: "",
+        productsatus: "",
+        amount: "",
+        minamount: "",
+    });
+
+    const inputs = [
+        {
+            id: 1,
+            name: "productname",
+            type: "text",
+            placeholder: "Please enter the product’s name",
+            errorMessage:
+                "Product name should be 3-16 characters and shouldn't include any special character!",
+            label: "Product name",
+            pattern: "^[A-Za-z0-9]{3,16}$",
+            required: true,
+        },
+        {
+            id: 2,
+            name: "productsatus",
+            type: "textarea",
+            placeholder: "Please describe your products, their status, and what users will receive more than 15 words",
+            errorMessage: "It should be a meaningful description of your products!",
+            label: "Product status",
+            pattern: "^[A-Za-z0-9]{15,}$",
+            required: true,
+        },
+        {
+            id: 3,
+            name: "amount",
+            type: "number",
+            placeholder: "Enter the selling price for the product",
+            label: "Amount",
+            errorMessage: "Amount should be a number!",
+            pattern: "^[0-9]{1,}$",
+            required: true,
+        },
+        {
+            id: 4,
+            name: "minamount",
+            type: "number",
+            placeholder: "Enter the selling price for the product",
+            label: "Amount",
+            errorMessage: "Amount should be a number!",
+            pattern: "^[0-9]{1,}$",
+            required: true,
+        },
+    ];
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    return (
+        <div className="app">
+            <form onSubmit={handleSubmit}>
+                <h1>SELLER LISTING</h1>
+                {inputs.map((input) => (
+                    <FormInput
+                        key={input.id}
+                        {...input}
+                        value={values[input.name]}
+                        onChange={onChange}
+                    />
+                ))}
+                <button>SUBMIT</button>
+            </form>
+        </div>
+    );
+};
+
+export default ListingPage;
