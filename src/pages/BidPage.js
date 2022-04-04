09591b6dbab8f8cd06a8ca609f3f60f832733103
doanyennhi <s3880599@rmit.edu.vn>
2022-04-05 01:53:15 +0700

    Styling for input text and amount row, update bid amount when user enter

--- src/pages/BidPage.js
+++ src/pages/BidPage.js
@@ -1,22 +1,25 @@
 import Layout from "../components/Layout";
 import style from "./BidPage.module.css";
 import common from "../styles/common.module.css"
+import {useState} from "react";
 
 const BidPage = () => {
+  const [bid, setBid] = useState("0");
 
   return (
       <Layout header footer className={[style["container"], common["flex"]].join(" ")}>
         <h1>Place a Bid</h1>
         <p className={style["subtitle"]}>You must bid at least <span className={style["price"]}>100</span></p>
-        <div className={[common["flex"], style["input"]].join(" ")}>
-          <input type="text"/>
-          <div className={style["currency"]}>VND</div>
+
+        <div className={[common["flex"], style["input-container"]].join(" ")}>
+          <input type="text" className={style["input"]} onChange={e => setBid(e.target.value)}/>
+          <div className={[style["currency"], common["text-center"]].join(" ")}>VND</div>
         </div>
 
         <div className={[common["flex"], style["amount"]].join(" ")}>
           <div className={[common["flex"], style["amount-row"]].join(" ")}>
             <div>Your bid</div>
-            <div>200,000 VND</div>
+            <div>{bid} VND</div>
           </div>
 
           <div className={[common["flex"], style["amount-row"]].join(" ")}>
