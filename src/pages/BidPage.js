06764a37c0504fc945a612ad48ecd42f709c762f
doanyennhi <s3880599@rmit.edu.vn>
2022-04-05 01:24:45 +0700

    Add amount and styling for amount

--- src/pages/BidPage.js
+++ src/pages/BidPage.js
@@ -8,8 +8,32 @@ const BidPage = () => {
       <Layout header footer className={[style["container"], common["flex"]].join(" ")}>
         <h1>Place a Bid</h1>
         <p className={style["subtitle"]}>You must bid at least <span className={style["price"]}>100</span></p>
-        <input type="text"/>
-        <div>VND</div>
+        <div className={[common["flex"], style["input"]].join(" ")}>
+          <input type="text"/>
+          <div className={style["currency"]}>VND</div>
+        </div>
+
+        <div className={[common["flex"], style["amount"]].join(" ")}>
+          <div className={[common["flex"], style["amount-row"]].join(" ")}>
+            <div>Your bid</div>
+            <div>200,000 VND</div>
+          </div>
+
+          <div className={[common["flex"], style["amount-row"]].join(" ")}>
+            <div>Deposit fee</div>
+            <div>20,000 VND</div>
+          </div>
+
+          <div className={[common["flex"], style["amount-row"]].join(" ")}>
+            <div>Service fee</div>
+            <div>5,000 VND</div>
+          </div>
+
+          <div className={[common["flex"], style["amount-row"]].join(" ")}>
+            <div>Total amount</div>
+            <div>225,000 VND</div>
+          </div>
+        </div>
       </Layout>
   )
 }
