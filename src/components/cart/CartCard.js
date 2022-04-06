57559aed90236c8df33911b5ad2f850a4225c189
rider3458 <s3878480@rmit.edu.vn>
2022-04-06 15:23:37 +0700

    update styling, add a field to show bid status

--- src/components/cart/CartCard.js
+++ src/components/cart/CartCard.js
@@ -1,10 +1,19 @@
 import style from "./CartCard.module.css"
+import {useEffect, useState} from "react";
 
-const CartCard = ({image, name, price, type}) => {
+const CartCard = ({image, name, price, bid, highestBid}) => {
+    const [winning, setWinning] = useState(true)
+    const currency = "VND"
+    useEffect(() => {
+        if (highestBid > price) {
+            setWinning(false)
+        }
+    }, [price, highestBid])
+
     return (
         <div className={style.cardWrapper}>
             <div className={style.cardImgWrapper}>
-                <img src={(image)} className={style.cardImg} alt={"product"}/>
+                <img src={image ? image : "../media/icons/logo.png"} className={style.cardImg} alt={"product"}/>
             </div>
             <div className={style.cardContentContainer}>
                 <div className={style.cardContent}>
@@ -12,8 +21,15 @@ const CartCard = ({image, name, price, type}) => {
                         <h5 className={style.cardName}>{name}</h5>
                     </div>
                     <div className={style.cardPriceWrapper}>
-                        <p className={style.cardPrice}>{price}</p>
+                        <p className={style.cardPrice}>{price + " " + currency}</p>
                     </div>
+                    {bid ?
+                        <div className={style.cardStatusWrapper}>
+                            <p className={winning ? style.winningCard : style.overBiddenCard}>
+                                {winning ? "WINNING" : "HiGHEST BID: " + highestBid + " " + currency}
+                            </p>
+                        </div>
+                    : null}
                 </div>
                 <div className={style.removeBtnWrapper}>
                     <button className={style.removeBtn}>REMOVE</button>
--- src/pages/cart/CartPage.js
+++ src/pages/cart/CartPage.js
@@ -1,37 +1,85 @@
 import Layout from "../../components/Layout";
 import style from "./CartPage.module.css";
 import CartCard from "../../components/cart/CartCard";
-import {useState} from "react";
-let temp = []
-for (let i = 0; i < 3; i ++) {
-    temp.push("")
-}
+import {useEffect, useState} from "react";
+const bid = [
+    {
+        "id": 0,
+        "name": "No. 0",
+        "img": "",
+        "price": 45000,
+        "bid": true,
+        "highestBid": 55000
+    },
+    {
+        "id": 1,
+        "name": "No. 1",
+        "img": "",
+        "price": 14000,
+        "bid": true,
+        "highestBid": 14000
+    },
+]
 
-
+const buy = [
+    {
+        "id": 2,
+        "name": "No. 2",
+        "img": "",
+        "price": 2000,
+        "bid": false,
+        "highestBid": null,
+    },
+    {
+        "id": 3,
+        "name": "No. 3",
+        "img": "",
+        "price": 24000,
+        "bid": false,
+        "highestBid": null,
+    },
+    {
+        "id": 4,
+        "name": "No. 4",
+        "img": "",
+        "price": 60000,
+        "bid": false,
+        "highestBid": null,
+    },
+]
 const CartPage = () => {
-    const [bid, setBid] = useState(true)
+    const [bidBtn, setBidBtn] = useState(true)
+    const [temp, setTemp] = useState(bid)
+
+    useEffect(() => {
+        if (bidBtn === true) {
+            setTemp(bid)
+        } else {setTemp(buy)}
+    })
+
     return (
         <Layout header footer>
             <div className={style.container}>
                 <h1>Cart</h1>
                 <div className={style.bidOrBuyNowContainer}>
                     <div>
-                        <button className={!bid ? style.changeModeBtn : style.changeModeBtnSelected} disabled={bid} onClick={() => {setBid(true)}}>Bid</button>
+                        <button className={!bidBtn ? style.changeModeBtn : style.changeModeBtnSelected} disabled={bidBtn} onClick={() => {setBidBtn(true)}}>bidBtn</button>
                     </div>
                     <div>
-                        <button className={bid ? style.changeModeBtn : style.changeModeBtnSelected}  disabled={!bid} onClick={() => {setBid(false)}}>Buy Now</button>
+                        <button className={bidBtn ? style.changeModeBtn : style.changeModeBtnSelected}  disabled={!bidBtn} onClick={() => {setBidBtn(false)}}>Buy Now</button>
                     </div>
                 </div>
                 <div className={style.contentContainer}>
                     <div className={style.cartListing}>
                         {
-                            temp.map(() => {
+                            temp.map((card) => {
                                 return (
                                     <CartCard
-                                        image={"../media/icons/logo.png"}
-                                        name={"product"}
-                                        price={"100"}
-                                        type={"bid"}
+                                        image={card.img}
+                                        name={card.name}
+                                        price={card.price}
+                                        bid={card.bid}
+                                        highestBid={card.highestBid}
                                     />
                                 )
                             })
@@ -51,13 +99,13 @@ const CartPage = () => {
                                 <div className={style.summaryFieldHeading}>Shipping charges</div>
                                 <div className={style.summaryFieldValue}></div>
                             </div>
-                            {bid ?
+                            {bidBtn ?
                                 <div className={style.summaryField}>
                                     <div className={style.summaryFieldHeading}>Deposit</div>
                                     <div className={style.summaryFieldValue}></div>
                                 </div>
                             : null}
-                            {bid ?
+                            {bidBtn ?
                                 <div className={style.summaryField}>
                                     <div className={style.summaryFieldHeading}>Service Fee</div>
                                     <div className={style.summaryFieldValue}>5000</div>
@@ -68,8 +116,8 @@ const CartPage = () => {
                             <div className={style.summaryField}>
                                 <div className={style.summaryFieldHeading}>Coupon</div>
                                 <div className={style.summaryFieldValueAsInput}>
-                                    <input type={"text"} />
-                                    <button>APPLY</button>
+                                    <input className={style.summaryFieldCouponInput} type={"text"} />
+                                    <button className={style.summaryFieldCouponBtn}>Apply</button>
                                 </div>
                             </div>
                         </div>
