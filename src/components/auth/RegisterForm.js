4174f8cb10c6e35de4942dc60b49c6912ce1793e
Hoang Linh <hlinh.jobs@gmail.com>
2022-04-10 10:53:49 +0700

    set up sign in + sign up function and push data to firestore

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,103 +1,129 @@
 import Layout from "../../components/Layout";
 import style from "./RegisterForm.module.css";
-import {useState} from "react";
+import { useState } from "react";
+import { signUp } from "../../api/authentication";
 
 const RegisterForm = () => {
-    const [emailOrPhoneNumber, setEmailOrPhoneNumber] = useState("");
-    const [fullName, setFullName] = useState("");
-    const [password, setPassword] = useState("");
-    const [verifyPassword, setVerifyPassword] = useState("");
+  const [emailOrPhoneNumber, setEmailOrPhoneNumber] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
 
-    const [checkedFirst, setCheckedFirst] = useState(false);
-    const [checkedSecond, setCheckedSecond] = useState(false);
+  const [checkedFirst, setCheckedFirst] = useState(false);
+  const [checkedSecond, setCheckedSecond] = useState(false);
 
-    const handleChangeFirst = () => {
-        setCheckedFirst(!checkedFirst);
-    };
+  const handleChangeFirst = () => {
+    setCheckedFirst(!checkedFirst);
+  };
 
-    const handleChangeSecond = () => {
-        setCheckedSecond(!checkedSecond);
-    };
-    const Checkbox = ({label, value, onChange}) => {
-        return (
-            <label>
-                <input type="checkbox" checked={value} onChange={onChange}/>
-                {label}
-            </label>
-        );
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert("Form submitted");
-    }
-
-    const handleReset = () => {
-        Array.from(document.querySelectorAll("input")).forEach(
-            input => (input.value = "")
-        );
-        this.setState({
-            itemValues: [{}]
-        });
-    };
-
+  const handleChangeSecond = () => {
+    setCheckedSecond(!checkedSecond);
+  };
+  const Checkbox = ({ label, value, onChange }) => {
     return (
-        <Layout className={style["register-container"]} header footer>
-            <div className={style["container"]}>
-            <form className={style["form"]} onSubmit={handleSubmit}>
-                <h1 className={style["h1"]}>Register</h1>
-                <img
-                    src={"../../media/icons/logo.png"}
-                    className={style["logo"]}
-                    alt={"Kronicle logo"}
-                />
-                <p className={style["p"]}>Already a member? <a href="#" className={style["a"]}>Sign in.</a></p>
-                <label className={style["label"]}>Email or Phone number</label>
-                <input
-                    type="text"
-                    value={emailOrPhoneNumber}
-                    onChange={(e) => setEmailOrPhoneNumber(e.target.value)}
-                    className={style["input"]}
-                />
-                <label className={style["label"]}>Full Name</label>
-                <input
-                    type="text"
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
-                    className={style["input"]}
-                />
-                <label className={style["label"]}>Password</label>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className={style["input"]}
-                />
-                <label className={style["label"]}>Verify Password</label>
-                <input
-                    type="password"
-                    value={verifyPassword}
-                    onChange={(e) => setVerifyPassword(e.target.value)}
-                    className={style["input"]}
-                />
-                <Checkbox
-                    label={<label className={style["label"]}>I agree to the &nbsp;<a href="#" className={style["a"]}>terms and conditions.</a></label>}
-                    value={checkedFirst}
-                    onChange={handleChangeFirst}
-                />
-                <Checkbox
-                    label={<label className={style["label"]}>Send me the latest deal.</label>}
-                    value={checkedSecond}
-                    onChange={handleChangeSecond}
-                />
-            </form>
-            <div className={style["button-wrapper"]}>
-                <button className={style["register-btn"]}>REGISTER</button>
-                <button type="submit" className={style["clear-btn"]} onClick={handleReset}>CLEAR</button>
-            </div>
-            </div>
-        </Layout>
+      <label>
+        <input type="checkbox" checked={value} onChange={onChange} />
+        {label}
+      </label>
     );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Form submitted");
+  };
+
+  const handleReset = () => {
+    Array.from(document.querySelectorAll("input")).forEach(
+      (input) => (input.value = "")
+    );
+    this.setState({
+      itemValues: [{}],
+    });
+  };
+
+  return (
+    <Layout className={style["register-container"]} header footer>
+      <div className={style["container"]}>
+        <form className={style["form"]} onSubmit={handleSubmit}>
+          <h1 className={style["h1"]}>Register</h1>
+          <img
+            src={"../../media/icons/logo.png"}
+            className={style["logo"]}
+            alt={"Kronicle logo"}
+          />
+          <p className={style["p"]}>
+            Already a member?{" "}
+            <a href="#" className={style["a"]}>
+              Sign in.
+            </a>
+          </p>
+          <label className={style["label"]}>Email or Phone number</label>
+          <input
+            type="text"
+            value={emailOrPhoneNumber}
+            onChange={(e) => setEmailOrPhoneNumber(e.target.value)}
+            className={style["input"]}
+          />
+          <label className={style["label"]}>Full Name</label>
+          <input
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            className={style["input"]}
+          />
+          <label className={style["label"]}>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={style["input"]}
+          />
+          <label className={style["label"]}>Verify Password</label>
+          <input
+            type="password"
+            value={verifyPassword}
+            onChange={(e) => setVerifyPassword(e.target.value)}
+            className={style["input"]}
+          />
+          <Checkbox
+            label={
+              <label className={style["label"]}>
+                I agree to the &nbsp;
+                <a href="#" className={style["a"]}>
+                  terms and conditions.
+                </a>
+              </label>
+            }
+            value={checkedFirst}
+            onChange={handleChangeFirst}
+          />
+          <Checkbox
+            label={
+              <label className={style["label"]}>Send me the latest deal.</label>
+            }
+            value={checkedSecond}
+            onChange={handleChangeSecond}
+          />
+        </form>
+        <div
+          className={style["button-wrapper"]}
+          onClick={() =>
+            signUp(emailOrPhoneNumber, fullName, password, verifyPassword)
+          }
+        >
+          <button className={style["register-btn"]}>REGISTER</button>
+          <button
+            type="submit"
+            className={style["clear-btn"]}
+            onClick={handleReset}
+          >
+            CLEAR
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default RegisterForm;
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain:  process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APPID
+  apiKey: "AIzaSyAGZzJlhVESFpwkUbZgj4PDVUFnakbJ24U",
+  authDomain: "kronicle-app.firebaseapp.com",
+  projectId: "kronicle-app",
+  storageBucket: "kronicle-app.appspot.com",
+  messagingSenderId: "951341387272",
+  appId: "1:951341387272:web:7cdeaba3bd6b6649088bbd",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
