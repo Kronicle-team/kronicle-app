32f7157829523a3db703eeeb355c66fdee0f30ed
rider3458 <s3878480@rmit.edu.vn>
2022-04-16 13:23:47 +0700

    revert

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAGZzJlhVESFpwkUbZgj4PDVUFnakbJ24U",
-  authDomain: "kronicle-app.firebaseapp.com",
-  projectId: "kronicle-app",
-  storageBucket: "kronicle-app.appspot.com",
-  messagingSenderId: "951341387272",
-  appId: "1:951341387272:web:7cdeaba3bd6b6649088bbd"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
