c539950e4c64630d96bdf09b69539150142437c0
minhthuhua72 <s3878667@rmit.edu.vn>
2022-04-19 20:28:05 +0700

    changed UI

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -5,12 +5,12 @@ import common from "../../styles/common.module.css"
 const NavBar = () => {
   return (
       <nav className={[style["nav"], common["flex"]].join(" ")}>
-          <Link to="/" className={style["active-link"]}>Home</Link>
-          <Link to="/all">All</Link>
-          <Link to="/cards/album-cards">Album cards</Link>
-          <Link to="/cards/trading-cards">Trading cards</Link>
-          <Link to="/cards/buy-now">Buy now</Link>
-          <Link to="/cards/bid">Bid</Link>
+          <Link to="/" className={[style["active-link"], style["links"]].join(" ")}>Home</Link>
+          <Link to="/all" className={style["links"]}>All</Link>
+          <Link to="/cards/album-cards" className={style["links"]}>Album cards</Link>
+          <Link to="/cards/trading-cards" className={style["links"]}>Trading cards</Link>
+          <Link to="/cards/buy-now" className={style["links"]}>Buy now</Link>
+          <Link to="/cards/bid" className={style["links"]}>Bid</Link>
       </nav>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 import React from "react";
 import CardShowCase from "../components/CardShowCase";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
-import { db } from "../config/firebase"
+import { db } from "../config/firebase";
 
 const HomePage = () => {
   const [latestBuyNow, setLatestBuyNow] = useState([]);
@@ -35,11 +35,12 @@ const HomePage = () => {
   if (latestBuyNow === undefined) {
     return <h1>Loading...</h1>
   }
+
   return (
       <Layout header footer>
             <div className={style.banner}>
                   <div className={style.bannerTextWrapper}>
-                      <img src={"../media/images/banner.png"} alt={"banner"} className={style.bannerBackground}/>
+                      {/*<img src={"../media/images/banner.png"} alt={"banner"} className={style.bannerBackground}/>*/}
                       <h1 className={style.bannerText}>The First Official Website For Trading K-Pop Photocards In Vietnam</h1>
                   </div>
             </div>
@@ -52,7 +53,6 @@ const HomePage = () => {
             <div className={style.latestBidsContainer}>
                 <CardShowCase name={"Latest Bids"} data={latestBid} link={""} useText={true} bid/>
             </div>
-
         <div className={style.latestProductsContainer}>
               <CardShowCase name={"Latest Products"} data={latestBuyNow} link={"/all"} useText={true} />
         </div>
