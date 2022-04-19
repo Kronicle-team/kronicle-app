79e32f1b4348930dda13280bc62acf60a5bccf36
Hoang Linh <hlinh.jobs@gmail.com>
2022-04-19 20:59:32 +0700

    - add sell bid

--- src/components/profile/SellerProfile.js
+++ src/components/profile/SellerProfile.js
@@ -3,11 +3,12 @@ import style from "./SellerProfile.module.css";
 import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../config/firebase";
+import { Link } from "react-router-dom";
 
 const SellerProfile = () => {
   const [data, setData] = useState({});
   const fetchData = async () => {
-    getDoc(doc(db, "users", "5FrihL0CgOch4KffrjTvlKVKNOO2")).then((docSnap) => {
+    getDoc(doc(db, "users", "HR6QlZUlyacmra6d3inTTrbw8sf2")).then((docSnap) => {
       if (docSnap.exists()) {
         setData(docSnap.data());
       } else {
@@ -29,14 +30,23 @@ const SellerProfile = () => {
           />
           <div className={style["name"]}>{data.fullName}</div>
           <div className={style["info"]}>
-            <p>100% new authenticated cards</p>
-            <p>I live in Korea</p>
+            <p>{data.aboutMe}</p>
           </div>
 
           <div className={style["button-wrapper"]}>
             <button className={style["follow-button"]}>Follow</button>
             <button className={style["message-button"]}>Message</button>
           </div>
+        </div>
+        <div className={style["sell-container"]}>
+          <nav className={style["nav"]}>
+            <Link to="/" className={style["active-link"]}>
+              Sell
+            </Link>
+            <Link to="/" className={style["option"]}>
+              Bid
+            </Link>
+          </nav>
         </div>
       </div>
     </Layout>
