a86bd9e06edc97877ef4d7b4864c95f041111bde
Hoang Linh <hlinh.jobs@gmail.com>
2022-04-20 14:49:00 +0700

    add banner to homepage and styling H1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import style from "./HomePage.module.css";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import CardShowCase from "../components/CardShowCase";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
@@ -11,21 +11,27 @@ const HomePage = () => {
   const [latestBid, setLatestBid] = useState([]);
 
   useEffect(() => {
-    const q = query(collection(db, "listing"), where("product_pricing", "==", "buy now"));
+    const q = query(
+      collection(db, "listing"),
+      where("product_pricing", "==", "buy now")
+    );
     onSnapshot(q, (querySnapshot) => {
       const cards = [];
       querySnapshot.forEach((doc) => {
-        cards.push({...doc.data(), id: doc.id});
+        cards.push({ ...doc.data(), id: doc.id });
       });
       setLatestBuyNow(cards);
       console.log(latestBuyNow);
     });
 
-    const q2 = query(collection(db, "listing"), where("product_pricing", "==", "bid now"));
+    const q2 = query(
+      collection(db, "listing"),
+      where("product_pricing", "==", "bid now")
+    );
     onSnapshot(q2, (querySnapshot) => {
       const bidCards = [];
       querySnapshot.forEach((doc) => {
-        bidCards.push({...doc.data(), id: doc.id});
+        bidCards.push({ ...doc.data(), id: doc.id });
       });
       setLatestBid(bidCards);
       console.log(latestBid);
@@ -33,31 +39,51 @@ const HomePage = () => {
   }, []);
 
   if (latestBuyNow === undefined) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
   return (
-      <Layout header footer>
-            <div className={style.banner}>
-                  <div className={style.bannerTextWrapper}>
-                      {/*<img src={"../media/images/banner.png"} alt={"banner"} className={style.bannerBackground}/>*/}
-                      <h1 className={style.bannerText}>The First Official Website For Trading K-Pop Photocards In Vietnam</h1>
-                  </div>
-            </div>
-            <div className={style.qrBannerContainer}>
-                  <div className={style.qrBannerText}>TRY OUR CARD DETECTION WEB APP!</div>
-                  <div className={style.qrBanner}>
-                      <img src={"../media/images/qr.png"} alt={"qr"} className={style.qr}/>
-                  </div>
-            </div>
-            <div className={style.latestBidsContainer}>
-                <CardShowCase name={"Latest Bids"} data={latestBid} link={""} useText={true} bid/>
-            </div>
-        <div className={style.latestProductsContainer}>
-              <CardShowCase name={"Latest Products"} data={latestBuyNow} link={"/all"} useText={true} />
-        </div>
-      </Layout>
-  )
-}
+    <Layout header footer>
+      <div className={style.banner}>
+        <div className={style.bannerTextWrapper}>
+          <img
+            src={"../media/images/svt-banner.jpg"}
+            alt={"banner"}
+            className={style.bannerBackground}
+          />
 
-export default HomePage
+          <h1 className={style.bannerText}>
+            The First Official Website For Trading K-Pop Photocards In Vietnam
+          </h1>
+        </div>
+      </div>
+      <div className={style.qrBannerContainer}>
+        <div className={style.qrBannerText}>
+          TRY OUR CARD DETECTION WEB APP!
+        </div>
+        <div className={style.qrBanner}>
+          <img src={"../media/images/qr.png"} alt={"qr"} className={style.qr} />
+        </div>
+      </div>
+      <div className={style.latestBidsContainer}>
+        <CardShowCase
+          name={"Latest Bids"}
+          data={latestBid}
+          link={""}
+          useText={true}
+          bid
+        />
+      </div>
+      <div className={style.latestProductsContainer}>
+        <CardShowCase
+          name={"Latest Products"}
+          data={latestBuyNow}
+          link={"/all"}
+          useText={true}
+        />
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;
