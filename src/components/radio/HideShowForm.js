fee842825ee65b2ed392b333305bb702d55905c7
Thu Tran <ngocanhthu20102002@gmail.com>
2022-04-21 15:44:35 +0700

    added current time

--- src/components/radio/HideShowForm.js
+++ src/components/radio/HideShowForm.js
@@ -1,12 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import style from "../form/ListingForm.module.css";
 
 // Use React Hook
-function Radio ({}) {
-  const [status, setStatus] = useState(0) // 0: no show, 1: show price, 2: show min price.
-  const [category, setCategory] = useState("")
-  const [product_pricing, setProductPricing] = useState("buy now")
-  const [price, setPrice] = useState("")
+function Radio({ setCategoryFunction, setProductPricingFunction, setPriceFunction }) {
+  const [status, setStatus] = useState(0); // 0: no show, 1: show price, 2: show min price.
+
 
   const radioHandler = (status) => {
     setStatus(status);
@@ -15,16 +13,24 @@ function Radio ({}) {
   const [focused, setFocused] = useState(false);
   const handleFocus = (e) => {
     setFocused(true);
-  }
-
+  };
 
   function displaySellingPrice() {
     if (status === 1) {
       return (
-
         <div className={style["form-input"]}>
-          <label className={style["label-listing"]} htmlFor="sellingPrice">Selling Price*</label>
-          <input name="price" type="number" className={style["input-listing"]} id="sellingPrice" required={true} placeholder="Selling Price" onChange={e => setPrice(e.target.value)}/>
+          <label className={style["label-listing"]} htmlFor="sellingPrice">
+            Selling Price*
+          </label>
+          <input
+            name="price"
+            type="number"
+            className={style["input-listing"]}
+            id="sellingPrice"
+            required={true}
+            placeholder="Selling Price"
+            onChange={(e) => setPriceFunction(e.target.value)}
+          />
         </div>
       );
     }
@@ -34,8 +40,18 @@ function Radio ({}) {
     if (status === 2) {
       return (
         <div className={style["form-input"]}>
-          <label className={style["label-listing"]} htmlFor="minPrice">Min Price*</label>
-          <input name="price" type="number" className={style["input-listing"]} id="minPrice" required={true} placeholder="Min Price" onChange={e => setPrice(e.target.value)}/>
+          <label className={style["label-listing"]} htmlFor="minPrice">
+            Min Price*
+          </label>
+          <input
+            name="price"
+            type="number"
+            className={style["input-listing"]}
+            id="minPrice"
+            required={true}
+            placeholder="Min Price"
+            onChange={(e) => setPriceFunction(e.target.value)}
+          />
         </div>
       );
     }
@@ -48,11 +64,21 @@ function Radio ({}) {
         <div className={style["radio-container-listing"]}>
           <div className={style["radio-align"]}>
             <label className={style["radio-label"]}>ALBUM CARD</label>
-            <input value="album photocard" type="radio" name="category" onChange={e => setCategory(e.target.value)}/>
+            <input
+              value="album photocard"
+              type="radio"
+              name="category"
+              onChange={(e) => setCategoryFunction(e.target.value)}
+            />
           </div>
           <div className={style["radio-align"]}>
             <label className={style["radio-label"]}>TRADING CARD</label>
-            <input value="photocard" type="radio" name="category" onChange={e => setCategory(e.target.value)}/>
+            <input
+              value="photocard"
+              type="radio"
+              name="category"
+              onChange={(e) => setCategoryFunction(e.target.value)}
+            />
           </div>
         </div>
         <span className={style["error-message"]}>Please choose a category</span>
@@ -63,19 +89,34 @@ function Radio ({}) {
         <div className={style["radio-container-listing"]}>
           <div className={style["radio-align"]}>
             <label className={style["radio-label"]}>BUY NOW</label>
-            <input value="buy now" type="radio" name="release" checked={status === 1} onClick={(e) => radioHandler(1)} onChange={e => setProductPricing(e.target.value)}/>
+            <input
+              value="buy now"
+              type="radio"
+              name="product_pricing"
+              checked={status === 1}
+              onClick={(e) => radioHandler(1)}
+              onChange={(e) => setProductPricingFunction(e.target.value)}
+            />
           </div>
           <div className={style["radio-align"]}>
             <label className={style["radio-label"]}>BID NOW</label>
-            <input value="bid" type="radio" name="release" checked={status === 2} onClick={(e) => radioHandler(2)} onChange={e => setProductPricing(e.target.value)}/>
+            <input
+              value="bid"
+              type="radio"
+              name="product_pricing"
+              checked={status === 2}
+              onClick={(e) => radioHandler(2)}
+              onChange={(e) => setProductPricingFunction(e.target.value)}
+            />
           </div>
         </div>
-        <span className={style["error-message"]}>Please choose a type of pricing</span>
+        <span className={style["error-message"]}>
+          Please choose a type of pricing
+        </span>
       </div>
       {status === 1 && displaySellingPrice()}
       {status === 2 && displayMinPrice()}
     </>
-
   );
 }
 
--- src/pages/listing/ListingPage.js
+++ src/pages/listing/ListingPage.js
@@ -4,25 +4,47 @@ import FormInput from "../../components/form/ListingForm";
 import style from "./ListingPage.module.css";
 import common from "../../styles/common.module.css";
 import Radio from "../../components/radio/HideShowForm";
-import {db} from "../../config/firebase.js";
+import { db } from "../../config/firebase.js";
 import { collection, addDoc } from "firebase/firestore";
-
 
 // const handleSubmit = (e) => {
 //     e.preventDefault();
 //     console.log(values);
 // };
 
-
 const ListingPage = () => {
   const [values, setValues] = useState({
     product_name: "",
     product_status: "",
     product_image: "",
-    category: "",
-    release: "",
-    price: ""
   });
+
+
+
+  const [category, setCategory] = useState("");
+  const [product_pricing, setProductPricing] = useState("");
+  const [price, setPrice] = useState("");
+
+  const setCategoryFunction = (category) => {
+    setCategory(category);
+  };
+
+  const setProductPricingFunction = (product_pricing) => {
+    setProductPricing(product_pricing);
+  };
+
+  const setPriceFunction = (price) => {
+    setPrice(price);
+  };
+
+  // Current state
+  const [curr , setCurr] = useState("");
+
+  // // Function to get time and date
+  // const getDate = () => {
+  //   const a = firestore.Timestamp.now().toDate().toString();
+  //   setCurr(a);
+  // }
 
   const pushProduct = async () => {
     try {
@@ -30,9 +52,11 @@ const ListingPage = () => {
         product_name: values.product_name,
         product_status: values.product_status,
         product_image: values.product_image,
-        category: values.category,
-        release: values.release,
-        price: values.price
+        category: category,
+        product_pricing: product_pricing,
+        price: price,
+        availability: "available",
+        date_time: Date().toLocaleString(),
       });
       console.log("Your review has been submitted!", docRef.id);
     } catch (e) {
@@ -42,7 +66,6 @@ const ListingPage = () => {
 
   const [fileUrl, setFileUrl] = useState(null);
   const [listing, setUsers] = useState([]);
-
 
   const inputs = [
     {
@@ -54,17 +77,18 @@ const ListingPage = () => {
         "Product name should be 3-16 characters and shouldn't include any special character!",
       label: "Product name*",
       pattern: "^[A-Za-z0-9]{3,16}$",
-      required: true
+      required: true,
     },
     {
       id: 2,
       name: "product_status",
       type: "textarea",
-      placeholder: "Please describe product's status, and what users will receive more than 15 characters",
+      placeholder:
+        "Please describe product's status, and what users will receive more than 15 characters",
       errorMessage: "It should be a meaningful description of your products!",
       label: "Product status*",
       pattern: "^.{15,}$",
-      required: true
+      required: true,
     },
     {
       id: 3,
@@ -73,20 +97,18 @@ const ListingPage = () => {
       placeholder: "Enter the selling price for the product",
       label: "Upload the product's image*",
       errorMessage: "Please input your product's image'",
-      required: true
-    }
+      required: true,
+    },
   ];
-
 
   const onChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
-
   return (
     <Layout header footer>
       <div className={[style["app"], common["flex"]].join(" ")}>
-        <form  className={style["listing-form"]}>
+        <form className={style["listing-form"]}>
           <h1 className={style["listing-title"]}>SELLER LISTING</h1>
           {inputs.map((input) => (
             <FormInput
@@ -94,15 +116,28 @@ const ListingPage = () => {
               {...input}
               value={values[input.name]}
               onChange={onChange}
-            />))}
+            />
+          ))}
           <Radio
             name="radio"
             value={values.radio}
             onChange={onChange}
+            setRadio={[setCategoryFunction, setProductPricingFunction, setPriceFunction]}
           />
+          <h1>{curr}</h1>
         </form>
         <div className={style["listing-btn-container"]}>
-          <button className={style["listing-btn"]} onClick={()=> pushProduct()}>SUBMIT</button>
+          <button
+            className={style["listing-btn"]}
+            onClick={() => {
+              pushProduct().then(r => {
+                console.log(r);
+              });
+              console.log();
+            }}
+          >
+            SUBMIT
+          </button>
         </div>
       </div>
     </Layout>
