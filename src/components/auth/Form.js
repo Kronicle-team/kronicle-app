f6412ea3bc752037b76451f8ab43fa060bd0071f
minhthuhua72 <s3878667@rmit.edu.vn>
2022-04-29 13:29:43 +0700

    add img upload to form

--- src/components/auth/Form.js
+++ src/components/auth/Form.js
@@ -1,22 +1,43 @@
 import Layout from "../../components/Layout";
 import style from "./Form.module.css";
-import { useState } from "react";
-import { pushData, signUp } from "../../api/authentication";
+import React, { useState } from "react";
+import { pushData } from "../../api/authentication";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCamera
+} from "@fortawesome/free-solid-svg-icons";
 
 const Form = () => {
+  const [image, setImage] = useState(null);
+  const [isDisplayed, setDisplay] = useState(false);
+  const [imageData, setImageData] = useState(null);
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
   const [address, setAddress] = useState("");
   const [aboutMe, setAboutMe] = useState("");
 
+  const onChangePicture = e => {
+    if (e.target.files[0]) {
+      setDisplay(true);
+      setImage(e.target.files[0]);
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        setImageData(reader.result);
+      });
+      reader.readAsDataURL(e.target.files[0]);
+    } else {
+      setDisplay(false);
+    }
+  };
+
   const handleReset = () => {
     Array.from(document.querySelectorAll("input")).forEach(
       (input) => (input.value = "")
     );
     this.setState({
-      itemValues: [{}],
+      itemValues: [{}]
     });
   };
   let navigate = useNavigate();
@@ -25,15 +46,43 @@ const Form = () => {
     <Layout className={style["register-container"]} header footer>
       <div className={style["container"]}>
         <form className={style["form"]}>
-          <img
-            src={"../../media/icons/logo.png"}
-            className={style["logo"]}
-            alt={"Kronicle logo"}
-          />
-
           <h1 className={[style["h1"], style["extra-info"]].join(" ")}>
             Tell us more about yourself
           </h1>
+
+          <div className={style["imgUpload-section"]}>
+            <div className={style["file-input"]}>
+              {isDisplayed ? (
+                <div className={style["previewImage"]}>
+                  {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                  <img className={style["profileImage"]} src={imageData} />
+                  <label htmlFor={"profileImageUpload"}>
+                    <FontAwesomeIcon icon={faCamera} className={style["upload-icon-display"]} />
+                  </label>
+                </div>
+
+              ) : (
+                <label htmlFor={"profileImageUpload"}>
+
+                  <FontAwesomeIcon icon={faCamera} className={style["upload-icon"]} />
+
+
+                </label>
+              )}
+            </div>
+
+            <label htmlFor={"profileImageUpload"} className={style["label"]}>Upload Profile Picture</label>
+
+            <input
+              type="file"
+              name="profileImageUpload"
+              id="profileImageUpload"
+              className={style["input"]}
+              onChange={onChangePicture}
+            />
+
+          </div>
+
 
           <div className={style["name-wrapper"]}>
             <div>
@@ -71,10 +120,10 @@ const Form = () => {
           />
           <label className={style["label"]}>About Me</label>
           <textarea
-              value={aboutMe}
-              onChange={(e) => setAboutMe(e.target.value)}
-              className={[style["input"], style["input-aboutme"]].join(" ")}
-              rows={5}
+            value={aboutMe}
+            onChange={(e) => setAboutMe(e.target.value)}
+            className={[style["input"], style["input-aboutme"]].join(" ")}
+            rows={5}
           />
         </form>
         <div
@@ -92,9 +141,11 @@ const Form = () => {
             CLEAR
           </button>
         </div>
+        ;
       </div>
     </Layout>
-  );
+  )
+    ;
 };
 
 export default Form;
