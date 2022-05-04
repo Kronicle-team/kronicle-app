3953daead7608fc1e32a83b02a6c03e11c6c5ba4
rider3458 <s3878480@rmit.edu.vn>
2022-05-04 19:08:07 +0700

    required-field function for listing page alert when all required fields are not filled

--- src/pages/listing/ListingPage.js
+++ src/pages/listing/ListingPage.js
@@ -15,6 +15,8 @@ const ListingPage = () => {
   const [image, setImage] = useState(null);
   const [url, setUrl] = useState("");
 
+  const required = [category, product_pricing, price, image, url]
+
   const [values, setValues] = useState({
     product_name: "",
     product_status: "",
@@ -158,10 +160,19 @@ const ListingPage = () => {
           <button
             className={style["listing-btn"]}
             onClick={() => {
+              let allRequiredFieldsFilled = true
+              required.map((field) => {
+                if (field === "") {allRequiredFieldsFilled = false
+                }
+              })
+              if (allRequiredFieldsFilled) {
               handleUpload();
               pushProduct().then((r) => {
                 console.log(r);
               });
+              } else {
+                alert("Please fill in the required fields!")
+              }
             }}
           >
             SUBMIT
