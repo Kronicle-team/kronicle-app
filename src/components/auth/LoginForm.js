45c9400820e4a7e9f29192df33783013d3068755
doanyennhi <s3880599@rmit.edu.vn>
2022-05-05 13:30:19 +0700

    Prevent authenticated users from accessing login and register

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,12 +1,14 @@
 import Layout from "../../components/Layout";
 import style from "./Form.module.css";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { signIn } from "../../api/authentication";
 import { Link, useNavigate } from "react-router-dom";
+import {auth} from "../../config/firebase";
 
 const LoginForm = () => {
   const [emailOrPhoneNumber, setEmailOrPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,6 +16,21 @@ const LoginForm = () => {
   };
 
   let navigate = useNavigate();
+
+  useEffect(() => {
+    const unsub = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        setIsLoading(false)
+      } else {
+        navigate("/")
+      }
+    });
+    unsub()
+
+    return () => unsub()
+  }, []);
+
+  if (isLoading) {return <div/>}
 
   return (
     <Layout className={style["login-container"]} header footer>
