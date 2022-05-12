7835fd8f7ec6fd99917778af8bdb49c7d538cc72
doanyennhi <s3880599@rmit.edu.vn>
2022-05-12 15:58:07 +0700

    Remove redundant code

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,7 +6,6 @@
  *
  ***************************************************************************************/
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -15,10 +14,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
